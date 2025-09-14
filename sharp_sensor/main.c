569543d240a6e811386d9aa154f94af3a16262ce
John Cook-Martin <johncookmartin@gmail.com>
2025-09-14 12:33:58 -0400

    sharp sensor complete

--- sharp_sensor/main.c
+++ sharp_sensor/main.c
@@ -5,9 +5,16 @@
 #include <stdbool.h>
 #include <util/delay.h>
 
-uint16_t calculate_distance(uint16_t raw_v){
-
+uint16_t get_raw_average(const uint16_t *arr, int size){
+   uint32_t total = 0;
+   for (int i = 0; i < size; i++){
+      total += arr[i];
+   }
+   return total / size;
 }
+
+int volt_to_distance[35] = {0, 0, 0, 40, 30, 25, 20, 18, 16, 14, 12, 11, 10, 10, 9, 8, 8, 7, 7, 7, 6, 6, 5, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 3, 3};
+
 
 int main(void) {
    // Initialize
@@ -16,11 +23,17 @@ int main(void) {
    initADC(use_8_bit);
 
    transmitString("Welcome to Sharp Sensor Reader!");
+   int sample_size = 5;
    uint16_t current_v = 0;
 
    while(1){
-      uint16_t raw_v = analog(0, use_8_bit);
-      printDec(raw_v);
-      _delay_ms(50);
+      uint16_t raw_arr[10];
+      for(int i = 0; i < sample_size; i++){
+         raw_arr[i] = analog(0, use_8_bit);
+      }
+      uint16_t raw_adc = get_raw_average(raw_arr, sample_size);
+      uint16_t raw_v = (raw_adc * 5) / 100;
+      printDec(volt_to_distance[raw_v]);
+      _delay_ms(1000);
    }
 }
